a08cfc77bdd7b4750d53b2ee58a86b79fb5b3d0f
Matthew Larsen <utegrad@gmail.com>
2017-03-08 20:09:06 -0700

    working on empty filter for membership list view

--- src/py_event_organizer/scheduler/urls.py
+++ src/py_event_organizer/scheduler/urls.py
@@ -7,11 +7,14 @@ from .views.scheduler import UpdateOrganizationView, UpdateMembershipView, \
 app_name = 'scheduler'
 urlpatterns = [
     url(r'^$', view=home_page.as_view(), name='index'),
+
     url(r'^participant/(?P<pk>\d+)/managed_orgs/', view=MyManagedOrgsListView.as_view(),
         name='my_managed_orgs'),
     url(r'^participant/(?P<pk>\d+)/membership/', view=MyOrgsListView.as_view(), name='my_memberships' ),
+
     url(r'^membership/(?P<pk>\d+)/update/', view=UpdateMembershipView.as_view(),
         name='update_membership'),
+
     url('^organization/(?P<pk>\d+)/members/', view=OrganizationMembershipListView.as_view(),
         name='organization_membership'),
     url(r'^organization/(?P<pk>\d+)/update/', view=UpdateOrganizationView.as_view(),
