073c2fbbec1be6295b5c3d4ebc4b6792484ecff2
Matthew Larsen <utegrad@gmail.com>
2017-03-11 22:43:11 -0700

    working on adding members to organizations

--- src/py_event_organizer/scheduler/static/scheduler/js/add_organization_member.js
+++ src/py_event_organizer/scheduler/static/scheduler/js/add_organization_member.js
@@ -6,9 +6,11 @@
 
 /* context_data refers to JSON object given through the organization_members.html template*/
 $(function() {
-    $(".js-add-member").click(function () {
+
+
+    var loadForm = function () {
         $.ajax({
-            url: '/scheduler/organization/' + context_data.organization_id + '/add_member',
+            url: '/scheduler/organization/' + context_data.organization_id + '/add_member/',
             type: 'get',
             dataType: 'json',
             beforeSend: function() {
@@ -18,5 +20,33 @@ $(function() {
                 $("#modal-member .modal-content").html(data.html_form);
             }
         });
-    });
+    };
+
+    var saveForm = function () {
+        var form = $(this);
+        // debugger;
+        $.ajax({
+            url: form.attr("action"),
+            data: form.serialize(),
+            type: form.attr("method"),
+            dataType: 'json',
+            success: function(data){
+                if(data.form_is_valid){
+                    alert("Member added"); //placeholder
+                }
+                else {
+                    $("#modal-member .modal-content").html(data.html_form);
+                }
+            }
+        });
+        return false;
+    };
+
+    //add member to organization
+    $(".js-add-member").click(loadForm);
+    $("#modal-member").on("submit", ".js-member-add-form", saveForm);
+
 });
+
+
+
